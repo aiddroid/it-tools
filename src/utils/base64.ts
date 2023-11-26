a9b4e7789e0857f8fc3403e4804bc0719ddb3408
Allen <aiddroid@gmail.com>
2023-11-26 15:54:48 +0800

    feat:更新语言文件链接

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,7 +1,19 @@
+import Utf8 from 'crypto-js/enc-utf8';
+import Base64 from 'crypto-js/enc-base64';
+
 export { textToBase64, base64ToText, isValidBase64, removePotentialDataAndMimePrefix };
 
+function base64Encode(str: string) {
+  return Base64.stringify(Utf8.parse(str));
+}
+
+function base64Decode(str: string) {
+  return Utf8.stringify(Base64.parse(str));
+}
+
 function textToBase64(str: string, { makeUrlSafe = false }: { makeUrlSafe?: boolean } = {}) {
-  const encoded = window.btoa(str);
+  // const encoded = window.btoa(str);
+  const encoded = base64Encode(str);
   return makeUrlSafe ? makeUriSafe(encoded) : encoded;
 }
 
@@ -16,7 +28,9 @@ function base64ToText(str: string, { makeUrlSafe = false }: { makeUrlSafe?: bool
   }
 
   try {
-    return window.atob(cleanStr);
+    const decoded = base64Decode(cleanStr);
+    return decoded;
+    // return window.atob(cleanStr);
   }
   catch (_) {
     throw new Error('Incorrect base64 string');
@@ -35,9 +49,12 @@ function isValidBase64(str: string, { makeUrlSafe = false }: { makeUrlSafe?: boo
 
   try {
     if (makeUrlSafe) {
-      return removePotentialPadding(window.btoa(window.atob(cleanStr))) === cleanStr;
+      // return removePotentialPadding(window.btoa(window.atob(cleanStr))) === cleanStr;
+      return removePotentialPadding(base64Encode(base64Decode(cleanStr))) === cleanStr;
     }
-    return window.btoa(window.atob(cleanStr)) === cleanStr;
+
+    // return window.btoa(window.atob(cleanStr)) === cleanStr;
+    return base64Encode(base64Decode(cleanStr)) === cleanStr;
   }
   catch (err) {
     return false;
