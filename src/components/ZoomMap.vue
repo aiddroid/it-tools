5811b23952286d4bdf1a1ef46f13679a65e28e30
Allen <aiddroid@gmail.com>
2023-12-13 10:42:52 +0800

    fix:修复地图组件ZoomMap缩放控件样式

--- src/components/ZoomMap.vue
+++ src/components/ZoomMap.vue
@@ -20,19 +20,30 @@ const appTheme = useAppTheme();
 </script>
 
 <template>
-  <div>
+  <div class="map-container">
     <n-form-item label="Map" label-placement="left" label-width="110" :show-feedback="false">
       <img class="location-map" :src="mapImageUrl" alt="Geo location" referrerpolicy="no-referrer">
     </n-form-item>
     <br>
-    <n-form-item label="Zoom level" label-placement="left" label-width="110" :show-feedback="false">
-      <n-input-number v-model:value="zoomLevel" max="18" min="3" placeholder="Map zoom level (ex: 6)" w-full />
+    <n-form-item class="zoom-level-item" label="Zoom" label-style="color: #666" label-placement="left" label-width="110" :show-feedback="false">
+      <n-input-number class="zoom-input" v-model:value="zoomLevel" max="18" min="3" placeholder="Map zoom level (ex: 6)" w-full />
     </n-form-item>
   </div>
 </template>
 
 <style lang="less" scoped>
+.map-container {
+    position: relative;
+}
+.zoom-level-item {
+    position: absolute;
+    right: 10px;
+    bottom: 30px;
+}
 .location-map {
     width: 100%;
+}
+.zoom-input {
+    width: 80px;
 }
 </style>
